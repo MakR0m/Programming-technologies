e1348e1799c8afb58326bb88727c1b3ac883c584
Roman <Lamb_54@mail.ru>
2025-04-12 13:39:11 +0700

    PI из библиотеки

--- OOP/Abstraction/Circle.cs
+++ OOP/Abstraction/Circle.cs
@@ -9,14 +9,13 @@ namespace OOP.Abstraction
     internal class Circle : Shape
     {
         public double Radius {  get; set; }
-        public const double Pi = 3.14; //Лучше хранить в другом месте, например в файле.
 
         public Circle(double radius)
         {
             Radius = radius;
         }
 
-        public override double GetArea() => Pi * Radius * Radius;
+        public override double GetArea() => Math.PI * Radius * Radius;
 
     }
 }
