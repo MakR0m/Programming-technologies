af5425c0d09711c4adbbe42ec0ff9bf4a1e80f2d
Roman <Lamb_54@mail.ru>
2025-04-14 14:20:54 +0700

    Добавлен принцип разделения интерфейсов (хороший и плохой)

---
+++ SOLID/BADS/ISP/IWorker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.BADS.ISP
+{
+    internal interface IWorker
+    {
+        void Work();
+        void Eat();
+    }
+
+    public class HumanWorker : IWorker
+    {
+        public void Eat() => Console.WriteLine("Ест");
+
+        public void Work() => Console.WriteLine("Работает");
+    }
+
+    class RobotWorker : IWorker
+    {
+        public void Eat() => throw new NotImplementedException(); // Не может есть
+        
+        public void Work() => Console.WriteLine("Ест");
+    }
+
+    //Интерфейс навязывает реализацию, нужно разделить на 2 интерфейса
+}
