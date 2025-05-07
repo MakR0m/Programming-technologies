56fe652d35681b43c2f9883e8d1182f784128c2c
Roman <Lamb_54@mail.ru>
2025-05-07 14:24:23 +0700

    Добавлен проект для работы с EF Core. Установлены нугеты для работы EF Core и PostgreSQL. Параллельно развернут докер контейнер с PostgreSQL. Добавлена модель пользователя и DbContext. Создана миграция.

---
+++ EFPostgreSQL/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using EFPostgreSQL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFPostgreSQL.Data
+{
+    // Наследуемся от DbContext — базовый класс Entity Framework Core для работы с базой данных
+    public class AppDbContext : DbContext
+    {
+        // DbSet<User> представляет таблицу "Users" в базе данных.
+        // Каждый DbSet соответствует таблице, а тип T — это сущность (модель), строки которой мы храним.
+        // Через него мы выполняем запросы, добавление, обновление и удаление данных.
+        public DbSet<User> Users => Set<User>();
+
+        // Метод конфигурации контекста. Здесь указываем параметры подключения к базе.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Используем PostgreSQL в качестве провайдера базы данных.
+            // Здесь задаём строку подключения к контейнеру PostgreSQL.
+            options.UseNpgsql("Host=localhost;Port=5432;Database=ef_demo;Username=postgres;Password=postgres");
+        }
+    }
+}
---
+++ EFPostgreSQL/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFPostgreSQL.Models
+{
+    public class User  
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;  // null forgiving оператор. Гарантирует, что значение не будет null во время выполнения, даже если сейчас это не видно
+        public int Age { get; set; }
+    }
+}
