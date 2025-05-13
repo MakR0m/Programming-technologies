c49dde3e194bef043e03323483beaf00555cfb05
Roman <Lamb_54@mail.ru>
2025-05-13 11:44:12 +0700

    Добавлен еще пример с тестами

---
+++ TddPractice.Core/Services/InvoiceService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddPractice.Core.Services
+{
+    public class InvoiceService
+    {
+        public decimal CalculateTotal(decimal[] itemPrices, decimal discountPercent)
+        {
+            if (itemPrices == null || itemPrices.Length == 0)
+                throw new ArgumentOutOfRangeException(nameof(itemPrices), "Массив пуст");
+            if (discountPercent < 0 || discountPercent > 100)
+                throw new ArgumentOutOfRangeException(nameof(discountPercent), "Скидка должна быть больше 0");
+            decimal total = 0;
+            foreach (var item in itemPrices)
+            {
+                if (item < 0)
+                    throw new ArgumentOutOfRangeException(nameof(itemPrices), "Все элементы массива должны быть положительными");
+                total += item;
+            }
+            return Math.Round(total * (100 - discountPercent) / 100, 2, MidpointRounding.ToPositiveInfinity);
+        }
+    }
+}
---
+++ TddPractice.Tests/Services/InvoiceServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TddPractice.Core.Services;
+
+namespace TddPractice.Tests.Services
+{
+    public class InvoiceServiceTests
+    {
+        [Fact]
+        public void CalculateTotal_ReturnsCorrectDiscountedSum()
+        {
+            //Arrange
+            var service = new InvoiceService();
+            decimal[] items = { 100m, 50m, 50m };
+            decimal discount = 25;
+
+            //Act
+            decimal total = service.CalculateTotal(items, discount);
+
+            //Assert
+            Assert.Equal(150m, total);
+        }
+
+        public static IEnumerable<object[]> InvalidInvoiceData =>
+            new List<object[]>
+            {
+                new object[] {null, 10, "itemPrices"},
+                new object[] {new decimal[0],10, "itemPrices"},
+                new object[] {new decimal[] {-10m,5m}, 10, "itemPrices" },
+                new object[] {new decimal[] { 10m, 5m }, -5, "discountPercent" },
+                new object[] {new decimal[] { 10m, 5m }, 120, "discountPercent" }
+            };
+
+
+        [Theory]
+        [MemberData(nameof(InvalidInvoiceData))]
+        public void CalculateTotal_ThrowException_OnInvalidInput(decimal[] items, decimal discount, string expectedParam)
+        {
+            //Arrange
+            var service = new InvoiceService();
+
+            //Act
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(()=> service.CalculateTotal(items, discount));
+
+            //Assert
+            Assert.Equal(expectedParam,exception.ParamName);
+        }
+    }
+}
